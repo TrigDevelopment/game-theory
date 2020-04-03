3299361e32b84b0bf51581c8ee7d3a28e4f1bc84
ArtemkaKokorin <xx.artem-kokorin@mail.ru>
2020-04-03 12:58:59 +0300

    Init

---
+++ build.bat
@@ -0,0 +1,18 @@
+
+
+
+http-server &
+
+
+
+npx webpack --config webpack.config.js
+
+
+
+npm install npm@latest -g
+npm install
+npm update
+npm audit fix
+npx webpack --config webpack.config.js
+
+
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+﻿const path = require('path')
+module.exports = {
+  mode: 'development',
+  //mode: 'production',
+  entry: {
+    index: './src/init.js',
+  },
+  target: 'web',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+}
